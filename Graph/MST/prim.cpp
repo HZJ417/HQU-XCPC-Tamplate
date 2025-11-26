c8c6485d1bf73d4868a7cfbcb6d44748b99a61cd
HZJ <a2379912303@gmail.com>
2025-11-26 17:06:01 +0800

    udp in 25/11/26

--- Graph/MST/prim.cpp
+++ Graph/MST/prim.cpp
@@ -20,47 +20,56 @@ struct node{
 }e[M*2];
 
 int n,m,tot=0,cnt=0,ans=0;
-int dist[N],head[N];
-bool vis[N];
+int dist[N],head[N],vis[N];
 
 void add(int u,int v,int w){
-    e[++tot].v=v;
+    e[tot].v=v;
     e[tot].w=w;
     e[tot].nxt=head[u];
-    head[u]=tot;
+    head[u]=tot++;
 }
 
 void Prim(){
     memset(dist,inf,sizeof(dist));
     dist[1]=0;
+    for(int i=head[i];i!=-1;i=e[i].nxt){
+        dist[e[i].v]=min(dist[e[i].v],e[i].w);
+    }
     q.push({1,0});
     while(!q.empty()){
-        if(cnt>=n) break;
+        if(cnt>=n) {break;}
         auto [u,d]=q.top();
+        // cerr<<u<<' '<<d<<endl;
         q.pop();
         if(vis[u]) continue;
         vis[u]=1;
         ++cnt;
+        // cerr<<u<<':'<<endl;
         ans+=d;
-        for(int i=head[u];i;i=e[i].nxt){
+        for(int i=head[u];i!=-1;i=e[i].nxt){
             int v=e[i].v,w=e[i].w;
             if(w<dist[v]){
                 dist[v]=w;
                 q.push({v,w});
+                // cerr<<v<<' '<<w<<endl;
             }
         }
     }
 }
 
 int main(){
+    memset(head,-1,sizeof head);
     cin>>n>>m;
-    for(int i=1;i<=n;++i){
+    for(int i=1;i<=m;++i){
         int u,v,w;
         cin>>u>>v>>w;
+        // if(u==v) continue;
         add(u,v,w);
         add(v,u,w);
     }
+    // cerr<<"tot:"<<tot<<endl;
     Prim();
+    // cerr<<endl<<cnt<<' '<<ans<<endl;
     if(cnt==n){
         cout<<ans<<endl;
     }else{
